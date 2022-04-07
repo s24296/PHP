74fb90a813da7198f34c29509c362ea9f0a5f60a
Maciej Piotrowicz <91725451+s24296@users.noreply.github.com>
2022-04-07 16:13:17 +0200

    Add files via upload

---
+++ 1.2.php
@@ -0,0 +1,33 @@
+<?php
+
+$a=7;
+
+if($a==1)
+{
+    echo 'poniedzialek';
+}
+if($a==2)
+{
+    echo 'wtorek';
+}
+if($a==3)
+{
+    echo 'sroda';
+}
+if($a==4)
+{
+    echo 'czwartek';
+}
+if($a==5)
+{
+    echo 'piatek';
+}
+if($a==6)
+{
+    echo 'sobota';
+}
+if($a==7)
+{
+    echo 'niedziela';
+}
+?>
